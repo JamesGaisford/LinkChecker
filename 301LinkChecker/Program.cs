14bb5665ad1656be16dfae6015b5087ac803b641
JamesGaisford <james.gaisford@t-rw.com>
2015-06-17 22:13:29 +0100

    Improvements to logging and now recording 301s and other response codes.

--- 301LinkChecker/Program.cs
+++ 301LinkChecker/Program.cs
@@ -8,22 +8,77 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Linq;
 using System.IO;
+using System.Diagnostics;
 
 namespace LinkChecker
 {
     class Program
     {
+        static List<string> LogFilePaths = null;
+
         static void Main(string[] args)
         {
             DateTime logStamp = DateTime.Now;
-            var log = CreateLog(logStamp);
-            log.WriteLine("Old Url, Target Url, Response Code, Redirect Same as Target");
+            LogFilePaths = new List<string>();
 
             var TransformsFromFile = GetUrlTransformsFromFile();
             var results = GetUrlResults(TransformsFromFile, false);
 
-            //var notRedirected = results.Where(x => (int)x.Response.Result.StatusCode != 301);
-            foreach (var result in results)
+            var groupedResults = results.GroupBy(x => (int)x.Response.Result.StatusCode).ToList();
+            var notRedirected = groupedResults.Where(x => x.Key != 301);
+            var onlyRedirects = groupedResults.Where(x => x.Key == 301).FirstOrDefault();
+            LogErrorUrls(logStamp, notRedirected, results.Count);
+            LogRedirectedUrls(logStamp, onlyRedirects, results.Count);
+            Console.WriteLine("Opening log files......");
+            foreach (string LogFilePath in LogFilePaths)
+            {
+                Process.Start(LogFilePath);
+            }
+            Console.Read();
+           
+        }
+
+        private static void LogErrorUrls(DateTime logStamp, IEnumerable<System.Linq.IGrouping<int, UrlTransformResult>> notRedirected, int TotalRequests)
+        {
+            var log = CreateLog(logStamp, "Errors");
+            log.WriteLine("Old Url, Target Url, Response Code");
+            foreach (var group in notRedirected)
+            {
+                foreach (var result in group)
+                {
+                    if (result.Response.IsCanceled || result.Response.IsFaulted)
+                    {
+                        log.WriteLine(String.Format("<<<<<<< ERROR {0} >>>>>>>", result.SourceUrls.OldUrl));
+                    }
+                    else
+                    {
+                        log.WriteLine(String.Format("{0},{1},{2}", result.SourceUrls.OldUrl, result.SourceUrls.NewUrl, (int)result.Response.Result.StatusCode));
+                    }
+
+                }
+                
+                log.WriteLine("Total results with response code " + group.Key + " = " + group.Count().ToString());
+            }
+            if (notRedirected.Any())
+            {
+                Console.WriteLine("Total Requests = " + TotalRequests.ToString());
+            }
+            else
+            {
+                Console.WriteLine("No requests that were not redirected!");
+            }
+            LogFilePaths.Add(((FileStream)(log.BaseStream)).Name);
+            log.Close();
+        }
+
+        private static void LogRedirectedUrls(DateTime logStamp, System.Linq.IGrouping<int, UrlTransformResult> onlyRedirects, int TotalRequests)
+        {
+            var log = CreateLog(logStamp, "Redirects");
+            log.WriteLine("Old Url, Target Url, Response Code, Redirect Same as Target");
+            int SuccessCounter = 0;
+            List<UrlTransform> ThreeOOnes = onlyRedirects.Select(x => new UrlTransform { OldUrl = x.SourceUrls.OldUrl, NewUrl = x.SourceUrls.NewUrl }).ToList();
+            var redirectResults = GetUrlResults(ThreeOOnes, true);
+            foreach (var result in redirectResults)
             {
                 if (result.Response.IsCanceled || result.Response.IsFaulted)
                 {
@@ -31,35 +86,25 @@ namespace LinkChecker
                 }
                 else
                 {
-                    log.WriteLine(String.Format("{0},{1},{2}", result.SourceUrls.OldUrl, result.SourceUrls.NewUrl, (int)result.Response.Result.StatusCode));
+                    bool NewUrlAndActualRedirectUrlMatch = (result.Response.Result.RequestMessage.RequestUri.LocalPath.ToLower() == result.SourceUrls.NewUrl.ToLower());
+                    if (NewUrlAndActualRedirectUrlMatch)
+                    {
+                        SuccessCounter++;
+                    }
+                    log.WriteLine(String.Format("{0},{1},{2},{3}", result.SourceUrls.OldUrl, result.SourceUrls.NewUrl, (int)result.Response.Result.StatusCode, NewUrlAndActualRedirectUrlMatch.ToString()));
                 }
 
             }
-
-            //var onlyRedirects = results.Where(x => (int)x.Response.Result.StatusCode == 301);
-            //var redirectResults = GetUrlResults(onlyRedirects.Select(x => new UrlTransform{ OldUrl = x.SourceUrls.OldUrl, NewUrl = x.SourceUrls.NewUrl}).ToList(), true);
-            //foreach (var result in redirectResults)
-            //{
-            //    if (result.Response.IsCanceled || result.Response.IsFaulted)
-            //    {
-            //        log.WriteLine(String.Format("<<<<<<< ERROR {0} >>>>>>>", result.SourceUrls.OldUrl));
-            //    }
-            //    else
-            //    {
-            //        log.WriteLine(String.Format("{0},{1},{2},{3}", result.SourceUrls.OldUrl, result.SourceUrls.NewUrl, (int)result.Response.Result.StatusCode, (result.Response.Result.RequestMessage.RequestUri.LocalPath.ToLower() == result.SourceUrls.NewUrl.ToLower()).ToString()));
-            //    }
-
-            //}
-
-            log.WriteLine("Total Redirects = " + results.Count().ToString());
-            //log.WriteLine("Successful redirect count = " + onlyRedirects.Count().ToString());
+            Console.WriteLine("Total Requests = " + TotalRequests.ToString());
+            Console.WriteLine("Successful redirect count = " + SuccessCounter.ToString());
+            LogFilePaths.Add(((FileStream)(log.BaseStream)).Name);
             log.Close();
         }
 
-        private static StreamWriter CreateLog(DateTime logStamp)
+        private static StreamWriter CreateLog(DateTime logStamp, string LogFileSuffix)
         {
             StreamWriter log;
-            log = new StreamWriter(String.Format("logfile{0}.txt", logStamp.Ticks));
+            log = new StreamWriter(String.Format("{0}_{1}.csv", logStamp.Ticks, LogFileSuffix));
             return (log);
         }
 
